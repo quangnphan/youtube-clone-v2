48b4923023df1053c51ff57ac77eeb416e03c732
Quang Phan <qp2194@gmail.com>
2022-10-02 23:06:20 -0500

    feat: video page

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box, Typography } from "@mui/material";
+import { Videos } from "../components";
+import { fetchApi } from "../utils/fetchApi";
 
 const SearchFeed = () => {
+  const [videos, setVideos] = useState(null);
+  const {searchTerm}=useParams();
+  useEffect(() => {
+    setVideos(null);
+    fetchApi(`search?part=snippet&q=${searchTerm}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, [searchTerm]);
   return (
-    <div>SearchFeed</div>
-  )
-}
+    <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+        Search Results for: <span style={{ color: "#FC1503" }}>{searchTerm}</span>
+      </Typography>
+      <Videos videos={videos} />
+    </Box>
+  );
+};
 
-export default SearchFeed
+export default SearchFeed;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
-import { Stack } from '@mui/material';
-import { categories } from '../utils/constants';
-const Sidebar = () => {
-  return (
-    <div>Sidebar</div>
-  )
-}
+import React from "react";
+import { Stack } from "@mui/material";
+import { categories } from "../utils/constants";
 
-export default Sidebar
+const Sidebar = ({selectedCategory,setSelectedCategory}) => {
+  return (
+    <Stack
+      direction="row"
+      sx={{
+        overflowY: "auto",
+        height: { xs: "auto", md: "95%" },
+        flexDirection: { md: "column" },
+      }}
+    >
+      {categories.map((item) => (
+        <button
+          className="category-btn"
+          style={{
+            background: item.name === selectedCategory && "#FC1503",
+            color: "white",
+          }}
+          key={item.name}
+          onClick={()=>setSelectedCategory(item.name)}
+        >
+          <span style={{color: item.name === selectedCategory ? 'white' : 'red',marginRight: '15px'}}>{item.icon}</span>
+          <span style={{opacity: item.name === selectedCategory ? '1' : '0.8'}}>{item.name}</span>
+        </button>
+      ))}
+    </Stack>
+  );
+};
+
+export default Sidebar;
